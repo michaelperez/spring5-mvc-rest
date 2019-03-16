ff2ea275bc66760396dae70e4eeef743930ecc9f
michaelperez <michael.perez@yahoo.com>
2019-03-15 21:25:43 -0500

    Include Customer Service integration test using Spring DataJpaTest.  - only loads data context, other components must be loaded manually. See setUp() method where Bootstrap is manually initialized and run()method called.  Closes #2

--- src/main/java/com/perezma/spring5mvcrest/services/CustomerServiceImpl.java
+++ src/main/java/com/perezma/spring5mvcrest/services/CustomerServiceImpl.java
@@ -65,4 +65,20 @@ public class CustomerServiceImpl implements CustomerService {
         returnDto.setCustomerUrl("/api/v1/customers/" + savedCustomer.getId());
         return returnDto;
     }
+
+    @Override
+    public CustomerDTO patchCustomer(Long id, CustomerDTO customerDTO) {
+        return customerRepository.findById(id).map(customer -> {
+
+            if(customerDTO.getFirstname() != null){
+                customer.setFirstname(customerDTO.getFirstname());
+            }
+
+            if(customerDTO.getLastname() != null){
+                customer.setLastname(customerDTO.getLastname());
+            }
+
+            return customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+        }).orElseThrow(RuntimeException::new); //todo implement better exception handling;
+    }
 }
