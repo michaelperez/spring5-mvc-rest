43acaf253d9d58e6a1aed06b7c746640f9affe8b
michaelperez <michael.perez@yahoo.com>
2019-03-15 21:38:58 -0500

    Implement PATCH mapping for a customer.  Closes #3

--- src/main/java/com/perezma/spring5mvcrest/controllers/v1/CustomerController.java
+++ src/main/java/com/perezma/spring5mvcrest/controllers/v1/CustomerController.java
@@ -42,4 +42,10 @@ public class CustomerController {
                 HttpStatus.OK);
     }
 
+    @PatchMapping("/{id}")
+    public ResponseEntity<CustomerDTO> patchCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO){
+        return new ResponseEntity<CustomerDTO>(customerService.patchCustomer(id, customerDTO),
+                HttpStatus.OK);
+    }
+
 }
