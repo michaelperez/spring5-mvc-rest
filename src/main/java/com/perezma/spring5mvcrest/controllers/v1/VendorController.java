f890ba38d411edfab71b53ad9efbd48148ad586a
michaelperez <michael.perez@yahoo.com>
2019-03-16 21:37:13 -0500

    Implement getAllVendors in controller & test. Closes #8

--- src/main/java/com/perezma/spring5mvcrest/controllers/v1/VendorController.java
+++ src/main/java/com/perezma/spring5mvcrest/controllers/v1/VendorController.java
@@ -1,7 +1,11 @@
 package com.perezma.spring5mvcrest.controllers.v1;
 
+import com.perezma.spring5mvcrest.api.v1.models.VendorListDTO;
 import com.perezma.spring5mvcrest.services.VendorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,10 +16,14 @@ public class VendorController {
 
     private final VendorService vendorService;
 
-
     public VendorController(VendorService vendorService) {
         this.vendorService = vendorService;
     }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public VendorListDTO getVendorList() {
+        return vendorService.getAllVendors();
+    }
 
 }
---
+++ src/test/java/com/perezma/spring5mvcrest/controllers/v1/VendorControllerTest.java
@@ -0,0 +1,54 @@
+package com.perezma.spring5mvcrest.controllers.v1;
+
+import com.perezma.spring5mvcrest.api.v1.models.VendorDTO;
+import com.perezma.spring5mvcrest.api.v1.models.VendorListDTO;
+import com.perezma.spring5mvcrest.services.VendorService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = {VendorController.class})
+public class VendorControllerTest extends AbstractRestControllerTest {
+
+    @MockBean //provided by Spring Context
+            VendorService vendorService;
+
+    @Autowired
+    MockMvc mockMvc; //provided by Spring Context
+
+    VendorDTO vendorDTO_1;
+    VendorDTO vendorDTO_2;
+
+    @Before
+    public void setUp() throws Exception {
+        vendorDTO_1 = new VendorDTO("Vendor 1", VendorController.BASE_URL + "/1");
+        vendorDTO_2 = new VendorDTO("Vendor 2", VendorController.BASE_URL + "/2");
+    }
+
+    @Test
+    public void getVendorList() throws Exception {
+        VendorListDTO vendorListDTO = new VendorListDTO(Arrays.asList(vendorDTO_1, vendorDTO_2));
+
+        given(vendorService.getAllVendors()).willReturn(vendorListDTO);
+
+        mockMvc.perform(get(VendorController.BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.vendors", hasSize(2)));
+    }
+}
